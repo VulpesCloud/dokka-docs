f16c4d604ed109e3ad4d385a056e0c846bfc0272
TheCGuyGitHub <christimaibach1@gmail.com>
2024-11-20 14:19:22 +0100

    Add first files via upload

---
+++ scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":VulpesCloud-bridge:dokkaHtmlPartial/main":[],":VulpesCloud-api:dokkaHtmlPartial/main":[],":VulpesCloud-launcher:dokkaHtmlPartial/main":[],":VulpesCloud-node:dokkaHtmlPartial/main":[],":VulpesCloud-connector:dokkaHtmlPartial/main":[],":VulpesCloud-wrapper:dokkaHtmlPartial/main":[]}'
